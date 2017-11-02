7997f78a2bb512951a08b9a4a65c438054427b65
nbn minds <nbnminds@nbns-Mac-mini.local>
2017-11-02 12:09:11 +0530

    Initial Commit

---
+++ gitupproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitupproject
+//
+//  Created by nbn minds on 02/11/17.
+//  Copyright © 2017 NBN Minds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
